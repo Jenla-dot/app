25dc3ab5fcab1a02afce922b0337b5d0b129cf81
SamritUddam <samrituddam20@gmail.com>
2023-09-21 20:52:28 +0700

    Upload

---
+++ app/Http/Controllers/Admin/TeacherController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    public function index()
+    {
+        return view('back-end.teacher.index');
+    }
+    public function dashboard()
+    {
+        return view('back-end.dashboard.index');
+    }
+
+}
---
+++ resources/views/back-end/teacher/index.blade.php
@@ -0,0 +1,5 @@
+@extends('back-end.master')
+
+@section('content')
+    <h1>Hellloooo</h1>
+@endsection
