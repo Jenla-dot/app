1255e9f816cd71b857b3386f02d1afbd964c0429
SamritUddam <samrituddam20@gmail.com>
2023-09-22 20:57:34 +0700

    Update database

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ use App\Http\Controllers\Admin\TeacherController;
 
 Route::get('/', [TeacherController::class,'dashboard'])->name('admin.dashboard');
 Route::get('/admin/teacher', [TeacherController::class,'index'])->name('admin.teacher');
+Route::get('/admin/teacher/create', [TeacherController::class,'create'])->name('admin.teacher.create');
+
