7c89206cd53ed66321636490ce280c23a0a7bf77
Darika Huon <sileap120@gmail.com>
2023-09-25 10:29:02 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\Admin\TeacherController;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [TeacherController::class,'dashboard'])->name('admin.dashboard');
 Route::get('/admin/teacher', [TeacherController::class,'index'])->name('admin.teacher');
